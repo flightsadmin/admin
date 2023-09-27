49b2267aed8a4a3175f5a1255264bffa7c8edd98
George <mctapplications@gmail.com>
2023-09-27 18:54:28 +0300

    Initial Commit

--- src/Commands/FileHandler.php
+++ src/Commands/FileHandler.php
@@ -105,14 +105,14 @@ trait FileHandler
             $userUpdate = 
             <<<NAV
             public function likes() {
-                return \$this->belongsToMany(Post::class, \'post_like\')->withTimestamps();
+                return \$this->belongsToMany(Post::class, 'post_like')->withTimestamps();
             }
-
-            public function hasLiked(Post $post) {
-                return \$this->likes()->where(\'post_id\', \$post->id)->exists();
+            
+            public function hasLiked(Post \$post) {
+                return \$this->likes()->where('post_id', \$post->id)->exists();
             }
-            NAV;
-
+            NAV; 
+            
             $userHook = "}";
 
             // Find the position of the last occurrence of "];"
