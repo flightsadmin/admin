d25b0a6de034be80dc5e33e19929b1f52d51436a
George <mctapplications@gmail.com>
2023-10-04 08:43:40 +0300

    Initial Commit

--- resources/install/permissions/resources/views/livewire/admin/settings/setting.blade.php
+++ resources/install/permissions/resources/views/livewire/admin/settings/setting.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row col-md-12">
             <div class="card">
-                <div class="card-header d-flex" style="display: flex; justify-content: space-between; align-items: center;">
+                <div class="card-header" style="display: flex; justify-content: space-between; align-items: center;">
                     <h3 class="card-title">General Setting</h3>
                     <button type="submit" class="btn btn-sm btn-primary float-end" wire:click="updateSetting">
                         <i class="bi bi-save mr-1"></i> Save changes
@@ -16,9 +16,9 @@
                                 placeholder="Enter site name">
                         </div>
                         <div class="col-md-6 form-group">
-                            <label for="site_short_code">Site Short Code</label>
+                            <label for="siteShortCode">Site Short Code</label>
                             <input wire:model="state.site_short_code" type="text" class="form-control"
-                                id="site_short_code" placeholder="Enter site Short Code">
+                                id="siteShortCode" placeholder="Enter site Short Code">
                         </div>
                         <div class="col-md-6 form-group">
                             <label for="siteEmail">Site Email</label>
@@ -36,15 +36,12 @@
                                 placeholder="Enter footer text">
                         </div>
                         <div class="col-md-6 form-group">
-                            <label for="site_theme" class="form-label">Site Theme</label>
+                            <label for="siteTheme" class="form-label">Site Theme</label>
                             <select wire:model="state.site_theme" class="form-select">
                                 <option value="">Choose an option...</option>
                                 <option value="light">Light</option>
                                 <option value="dark">Dark</option>
                             </select>
-                            @error('state.site_theme')
-                                <div class="text-danger">{{ $message }}</div>
-                            @enderror
                         </div>
                         <div class="col-md-12 form-group">
                             <label for="site_description">Descripion</label>
@@ -57,11 +54,3 @@
         </div>
     </div>
 </section>
-
-@push('scripts')
-    <script>
-        document.getElementById('sidebarCollapse').addEventListener('change', function() {
-            document.body.classList.toggle('sidebar-collapse');
-        });
-    </script>
-@endpush
