112c7b42e9ed16ec37094094b04fe3606c141caa
George <mctapplications@gmail.com>
2023-10-22 13:32:10 +0300

    Initial Commit

--- resources/install/permissions/resources/views/livewire/shop/products/review.blade.php
+++ resources/install/permissions/resources/views/livewire/shop/products/review.blade.php
@@ -57,15 +57,7 @@
                                             <span class="fw-light text-end"> Created: {{ $item->created_at->format('F d, Y') }}</span>
                                         </div>
                                         <div class="mt-3">
-                                            <p class="font-light ms-4">I purchased my Tab S2 at Best Buy but I wanted
-                                                to
-                                                share my thoughts on Amazon. I'm not going to go over specs and
-                                                such
-                                                since you can read those in a hundred other places. Though I
-                                                will
-                                                say that the "new" version is preloaded with Marshmallow and now
-                                                uses a Qualcomm octacore processor in place of the Exynos that
-                                                shipped with the first gen.</p>
+                                            <p class="font-light ms-4">This is a product review.</p>
                                         </div>
                                     </div>
                                 @empty
