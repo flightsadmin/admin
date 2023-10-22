1a829bf7432e0b42cb0047bd7cbe30308da49354
George <mctapplications@gmail.com>
2023-10-22 13:47:54 +0300

    Initial Commit

--- config/config.php
+++ config/config.php
@@ -14,7 +14,7 @@ return [
     "footer_text" => "Scaling Shopping Experience",
 
     // Allow Social Login
-    "sociallogin" => false,
+    "sociallogin" => true,
 
     // Routes
     "adminRoute" => "admin",
